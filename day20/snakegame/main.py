a58cd3eafff3a9ac6c8e729d78b797108ac3524f
Andreas Steffensen <andreas.steffensen89@gmail.com>
2021-02-09 09:49:31 +0100

    change to wasd controls

--- day20/snakegame/main.py
+++ day20/snakegame/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,17 +11,41 @@ screen.title("Snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Scoreboard(0)
 
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+current_score = 0
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
     
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        current_score = current_score + 1
+        food.refresh()
+        snake.extend()
+        score.update(current_score)
 
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
 
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+   
 
 
 
